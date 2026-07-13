029ab82a18bb309e2abe78f3c12bf7c49337281d
Muhammed Irfan S <irfanmhdm123@gmail.com>
2026-07-13 10:13:42 +0530

    Navbar Completed

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,63 +1,75 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const ViewCars = () => {
+const View = () => {
   const [data, changeData] = useState([]);
 
   const fetchData = () => {
+    axios
+      .get("https://host-demo-app.onrender.com/api/donors")
+      .then((response) => {
+        changeData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-        axios.get("https://raw.githubusercontent.com/anishpdm/demo-json-api/refs/heads/main/car-data.json").then(
-            (response)=>{
-                changeData(response.data)
-            }
-        ).catch()
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  }
-  
-  useEffect(
-    ()=>{
-        fetchData()
-    },[]
-  )
   return (
     <div>
       <div className="container mt-4">
+        <h2 className="text-center mb-4">Blood Donors</h2>
+
         <div className="row">
+          {data.map((value) => {
+            return (
+              <div
+                className="col-12 col-sm-6 col-md-4 col-lg-4 g-3"
+                key={value.id}
+              >
+                <div className="card h-100 shadow">
+                  <div className="card-body">
+                    <h4 className="card-title text-center text-danger">
+                      {value.donor_name}
+                    </h4>
 
-          {data.map(
-            (value, index) => {
+                    <hr />
 
-              return (
+                    <p className="card-text">
+                      <strong>Age:</strong> {value.age}
+                      <br />
+                      <strong>Gender:</strong> {value.gender}
+                      <br />
+                      <strong>Blood Group:</strong> {value.blood_group}
+                      <br />
+                      <strong>Phone:</strong> {value.phone}
+                      <br />
+                      <strong>Email:</strong> {value.email}
+                      <br />
+                      <strong>City:</strong> {value.city}
+                      <br />
+                      <strong>Weight:</strong> {value.weight_kg} kg
+                      <br />
+                      <strong>Last Donation:</strong>{" "}
+                      {value.last_donation_date}
+                    </p>
 
-                <div className="col-12 col-sm-6 col-md-4 col-lg-4 g-3">
-                  <div className="card h-100">
-                    <img
-                      src={value.image}
-                      className="card-img-top"
-                      style={{ height: "300px", width : "350px", objectFit: "fill",padding:"10px" , marginLeft:"20px" }}
-                      alt="Items"
-                    />
-                    <div className="card-body text-center">
-                      <h5 className="card-title"><strong>{value.brand}</strong></h5>
-                      <p className="card-text">
-                       <strong> {value.model} <br />{value.fueltype}<br />{value.color}<br />{value.year}<br />
-                        {value.price}</strong>
-                      </p>
-                      <button className="btn btn-primary">Add to Cart</button>
-                    </div>
+                    <button className="btn btn-danger w-100">
+                      {value.blood_group} Donor
+                    </button>
                   </div>
                 </div>
-              )
-            }
-          )}
-
-
+              </div>
+            );
+          })}
         </div>
       </div>
-
     </div>
+  );
+};
 
-  )
-}
-
-export default View
+export default View;
